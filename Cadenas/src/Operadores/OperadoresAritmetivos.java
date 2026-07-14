b56908008653ed41b684ba021b98c0d48eac7e4c
Brayan Emilio Ortiz Ramirez <brayan.ortizr@imss.gob.mx>
2026-07-14 17:00:39 -0600

    nuevos archivos ejercicios curso java

---
+++ Cadenas/src/Operadores/OperadoresAritmetivos.java
@@ -0,0 +1,8 @@
+package Operadores;
+
+public class OperadoresAritmetivos {
+    public static void main(String[] args) {
+        System.out.println("*** Operadores Aritmeticos ***");
+        
+    }
+}
