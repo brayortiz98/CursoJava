d26247d933b2a5ab86c05f478d5db4d08f9cd2b4
Brayan Emilio Ortiz Ramirez <brayan.ortizr@imss.gob.mx>
2026-07-15 17:01:53 -0600

    cambios

--- Cadenas/src/Operadores/OperadoresAritmetivos.java
+++ Cadenas/src/Operadores/OperadoresAritmetivos.java
@@ -3,6 +3,7 @@ package Operadores;
 public class OperadoresAritmetivos {
     public static void main(String[] args) {
         System.out.println("*** Operadores Aritmeticos ***");
+        int a = 5, b = 3, resultado = 0;
         
     }
 }
