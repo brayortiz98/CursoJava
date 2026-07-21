2e8a38fa05f8fa8fbf642eb97f026cdb4ae11df0
Brayan Emilio Ortiz Ramirez <brayan.ortizr@imss.gob.mx>
2026-07-21 17:05:06 -0600

    Cambios Ejercicios Curso Java

--- Cadenas/src/Operadores/OperadoresAsignacion.java
+++ Cadenas/src/Operadores/OperadoresAsignacion.java
@@ -2,8 +2,14 @@ package Operadores;
 
 public class OperadoresAsignacion {
     public static void main(String[] args) {
-        System.out.println("**** Operadores de Asignacion");
+        System.out.println("**** Operadores de Asignacion ****");
         // Asignacion
+        int miNumero = 10;
+        int miNumero2;
+        miNumero2 = 15;
+        // Asignacion compuesto
+        // +=
+
         
     }
 }
